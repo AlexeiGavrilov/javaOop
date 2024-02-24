5e181bb175c102bdd3bcedf8cf00945e0efefd19
Alex <kccnbnb@mail.ru>
2024-02-24 22:34:00 +0300

    some fix

--- src/seminar1/Main.java
+++ src/seminar1/Main.java
@@ -10,10 +10,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class Main {
-/*    public static ArrayList<Character> team1 = new ArrayList<>();
-    public static ArrayList<Character> team2 = new ArrayList<>();
-     FillTeam.FillTeamRandomChar(team1, team2);
-    */ // То что я хотел бы реализовать
+
     public static void main(String[] args) {
         ArrayList<Character> team1 = new ArrayList<>();
         ArrayList<Character> team2 = new ArrayList<>();
